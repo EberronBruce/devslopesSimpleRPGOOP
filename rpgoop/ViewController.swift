0a71ca0b425d415886496537e79cddd0e4cfbd39
Bruce Burgess <bruceburgess67@yahoo.com>
2016-03-21 01:20:35 +0700

    added an attack button and connect some ui

--- rpgoop/ViewController.swift
+++ rpgoop/ViewController.swift
@@ -10,12 +10,56 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var printLbl: UILabel!
+    @IBOutlet weak var playerHpLbl: UILabel!
+    @IBOutlet weak var enemyHpLbl: NSLayoutConstraint!
+    @IBOutlet weak var enemyImage: UIImageView!
+    @IBOutlet weak var chestBtn: UIButton!
+    
+    var player: Player!
+    var enemy: Enemy!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        player = Player(name: "DirtyLaundry21", hp: 110, attackPwr: 20)
+        
+        generateRandomEnemy()
+        
+        playerHpLbl.text = "\(player.hp) HP"
+        
+    }
+    
+    func generateRandomEnemy() {
+        let rand = Int(arc4random_uniform(2))
+        
+        if rand == 0 {
+            enemy = Kimara(startingHp: 50, attackPwr:  12)
+        } else {
+            enemy = DevilWizard(startingHp: 50, attackPwr: 15)
+        }
+           
+        
+        
     }
 
-    
 
+    @IBAction func onChestTapped(sender: AnyObject) {
+        
+    }
+
+    @IBAction func attackTapped(sender: AnyObject) {
+        
+        
+    }
 }
+
+
+
+
+
+
+
+
 
