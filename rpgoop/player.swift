43ca7d0cc2d74bc8be5f753486a01b35b670a0fb
Bruce Burgess <bruceburgess67@yahoo.com>
2016-03-20 23:17:13 +0700

    Create character class

---
+++ rpgoop/player.swift
@@ -0,0 +1,13 @@
+//
+//  player.swift
+//  rpgoop
+//
+//  Created by Bruce Burgess on 3/20/16.
+//  Copyright © 2016 Red Raven Computing Studios. All rights reserved.
+//
+
+import Foundation
+
+class Player {
+    
+}
